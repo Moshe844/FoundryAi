f4200589f0851190a87e4211a78b9090cc31d2a3
Moshe844 <100170084+Moshe844@users.noreply.github.com>
2026-07-27 13:17:43 -0400

    Mount authoritative approval controls

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { DurableApprovalBanner } from "@/components/DurableApprovalBanner";
 import { DurableMissionAuthority } from "@/components/DurableMissionAuthority";
 import { WorkspaceShell } from "@/components/WorkspaceShell";
 
 export default function Home() {
   return (
     <DurableMissionAuthority>
+      <DurableApprovalBanner />
       <WorkspaceShell />
     </DurableMissionAuthority>
   );
